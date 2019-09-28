d732163511205b3bb37556a2e9221ddff84eb793
mthorning <matthewthorning@gmail.com>
2019-09-28 17:00:43 +0100

    added None, dep, dev, or peer to each dependency

--- src/types/detail.rs
+++ src/types/detail.rs
@@ -5,6 +5,7 @@ use std::io::{Error, ErrorKind, Read};
 use std::path::PathBuf;
 
 #[derive(Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
 struct PjsonDetail {
     name: String,
     version: String,
@@ -83,34 +84,56 @@ pub mod dependency_detail {
 
     use super::application_detail::AppDetail;
     use super::PjsonDetail;
+    use std::collections::HashMap;
     use std::io::Error;
     use std::path::{Path, PathBuf};
 
     #[derive(Debug)]
-    pub enum DepType<'a> {
-        Dependency(&'a str),
-        DevDependency(&'a str),
-        PeerDependency(&'a str),
+    pub enum DepType {
+        Dependency(String),
+        DevDependency(String),
+        PeerDependency(String),
     }
 
     /// Holds the name and version values from the package.json files.
     #[derive(Debug)]
-    pub struct DepDetail<'a> {
+    pub struct DepDetail {
         pub name: String,
         pub version: String,
         pub path: PathBuf,
-        pub is_direct_dep: Option<DepType<'a>>,
+        pub is_direct_dep: Option<DepType>,
     }
 
     impl DepDetail {
         /// Returns a new DepDetail type.
-        pub fn new<'a>(base_path: &Path, app_details: &'a AppDetail) -> Result<DepDetail<'a>, Error> {
+        pub fn new(base_path: &Path, app_details: &AppDetail) -> Result<DepDetail, Error> {
             let mut path = PathBuf::from(base_path);
             let pjson_details = super::get_pjson_details(&mut path)?;
             let PjsonDetail { name, version, .. } = pjson_details;
 
-            let is_direct_dep = Self::check_is_direct(name, app_details);
-            println!("{:?}", is_direct_dep);
+            // is direct dependency?
+            let mut is_direct_dep =
+                Self::check_is_direct(&name, &app_details.dependencies, DepType::Dependency);
+
+            // is direct devDependency?
+            if is_direct_dep.is_none() {
+                is_direct_dep = Self::check_is_direct(
+                    &name,
+                    &app_details.dev_dependencies,
+                    DepType::DevDependency,
+                );
+            }
+
+            // is direct peerDependency?
+            if is_direct_dep.is_none() {
+                is_direct_dep = Self::check_is_direct(
+                    &name,
+                    &app_details.peer_dependencies,
+                    DepType::PeerDependency,
+                );
+            }
+
+            println!("{} is {:?}", name, is_direct_dep);
 
             Ok(DepDetail {
                 path,
@@ -119,16 +142,22 @@ pub mod dependency_detail {
                 version,
             })
         }
-        fn check_is_direct<'a>(dep_name: &str, app_details: &'a AppDetail) -> Option<DepType<'a>> {
-            match app_details.dependencies {
-                Some(deps) => {
-                    match deps.get(dep_name) {
-                        Some(version) => return Some(DepType::Dependency(&version)),
-                        None => return None,
-                    };
+
+        fn check_is_direct<F>(
+            dep_name: &str,
+            app_dependencies: &Option<HashMap<String, String>>,
+            dep_type: F,
+        ) -> Option<DepType>
+        where
+            F: Fn(String) -> DepType,
+        {
+            match app_dependencies {
+                Some(deps) => match deps.get(dep_name) {
+                    Some(requested_version) => Some(dep_type(requested_version.to_string())),
+                    None => None,
                 },
-                None => return None,
-            };
+                None => None,
+            }
         }
     }
 }
