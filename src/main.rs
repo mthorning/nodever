e219e97d5f42a9ee8be047aa1f66ced39d83b8ec
mthorning <matthewthorning@gmail.com>
2019-09-29 19:26:27 +0100

    removed commented lines

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,6 @@ use types::application_detail::AppDetail;
 use types::cli::Cli;
 
 fn main() -> Result<(), ExitFailure> {
-    //let path = cli.path;
-    //let dependencies = cli.dependencies;
-    //let regex = Regex::new(&cli.filter)?;
-    //let _sort = cli.sort;
-
     let app_details = AppDetail::new(Cli::from_args())?;
     app_details.print()?;
 
