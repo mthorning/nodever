0cc0d68bf2cd81e6ba3baa71796a52475ffca862
Matt Thorning <matthewthorning@gmail.com>
2019-12-27 09:55:12 +0000

    wip - before refactor

--- src/print.rs
+++ src/print.rs
@@ -4,47 +4,51 @@ use prettytable::{cell, color, row, Attr, Cell, Row, Table};
 use regex::Regex;
 use std::io::{self, Error, Write};
 
-#[derive(Debug)]
-enum Mode {
-    PlainList,
-    DirectDepsList,
+#[derive(PartialEq)]
+enum Mode<'a> {
+    PlainList(Vec<&'a str>),
+    DirectDepsList(Vec<&'a str>),
 }
 
 pub fn print_details(app_details: &AppDetail) -> Result<(), Error> {
-    print_table(app_details);
+    let mode = get_mode(app_details);
+    print_table(&mode, app_details);
     print_completion_message(app_details)?;
     Ok(())
 }
 
-fn print_table(app_details: &AppDetail) {
-    let mode = get_mode(app_details);
+fn print_table(mode: &Mode, app_details: &AppDetail) {
     if !app_details.dependency_details.is_empty() {
         let mut table = Table::new();
-        print_table_rows(app_details, &mut table, mode);
+        add_table_rows(app_details, &mut table, mode);
         table.printstd();
     }
 }
 
 fn get_mode(app_details: &AppDetail) -> Mode {
     if app_details.args.direct_dep {
-        return Mode::DirectDepsList;
+        return Mode::DirectDepsList(vec!["Package", "Type", "PJSON", "Version"]);
     }
-    Mode::PlainList
+    Mode::PlainList(vec!["Package", "Version"])
 }
 
-fn print_table_rows(app_details: &AppDetail, table: &mut Table, mode: Mode) {
+fn add_table_rows(app_details: &AppDetail, table: &mut Table, mode: &Mode) {
     for detail in app_details.dependency_details.iter() {
         let mut row_vec = row![detail.name];
-        if app_details.args.direct_dep {
-            if let Some((dep_type, version)) = get_pjson_version(&detail.is_direct_dep) {
-                row_vec.add_cell(Cell::new(dep_type));
-                row_vec.add_cell(Cell::new(version));
-            }
+        if let Mode::DirectDepsList(_) = mode {
+            add_direct_deps_cols(&detail.is_direct_dep, &mut row_vec);
         }
         row_vec.add_cell(Cell::new(&detail.version));
 
-        format_row(detail, &mut row_vec, &mode);
+        format_row(&mut row_vec, detail, &mode);
         table.add_row(row_vec);
+    }
+}
+
+fn add_direct_deps_cols(is_direct_dep: &DepType, row_vec: &mut Row) {
+    if let Some((dep_type, version)) = get_pjson_version(is_direct_dep) {
+        row_vec.add_cell(Cell::new(dep_type));
+        row_vec.add_cell(Cell::new(version));
     }
 }
 
@@ -57,16 +61,16 @@ fn get_pjson_version<'a>(is_direct: &'a DepType) -> Option<(&'a str, &'a str)> {
     }
 }
 
-fn format_row(detail: &DepDetail, row_vec: &mut Row, mode: &Mode) {
+fn format_row(row_vec: &mut Row, detail: &DepDetail, mode: &Mode) {
     match mode {
-        Mode::PlainList => {
+        Mode::PlainList(_) => {
             if detail.is_direct_dep != DepType::None {
                 for cell in row_vec.iter_mut() {
                     cell.style(Attr::ForegroundColor(color::BLUE));
                 }
             }
         }
-        Mode::DirectDepsList => {
+        Mode::DirectDepsList(_) => {
             if let Some((_, pjson_version)) = get_pjson_version(&detail.is_direct_dep) {
                 let version_re = Regex::new(&detail.version).unwrap();
                 if version_re.is_match(&pjson_version) {
