66293af5b6047c06ff4a285347c768a091e61801
Matt Thorning <matthewthorning@gmail.com>
2021-07-06 21:03:59 +0100

    refactoring for lib

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -1,6 +1,7 @@
-#[macro_use] extern crate prettytable;
-mod node_module;
+#[macro_use]
+extern crate prettytable;
 mod cli;
+mod node_module;
 mod pjson_detail;
 mod semver;
 
@@ -8,18 +9,57 @@ use std::default::Default;
 use std::io::{self, Error, Write};
 use std::path::PathBuf;
 
-use which::which;
-use regex::Regex;
 use prettytable::Table;
+use regex::Regex;
+use which::which;
 
 pub use cli::Cli;
-pub use pjson_detail::PjsonDetail;
-pub use node_module::standard_module::StandardModule;
-pub use node_module::global_module::GlobalModule;
 pub use node_module::diffed_pair::DiffedPair;
+pub use node_module::global_module::GlobalModule;
+pub use node_module::standard_module::StandardModule;
+pub use pjson_detail::PjsonDetail;
 
+pub fn run_global() -> Result<(), Error> {
+    let base_path = get_node_modules_path(&get_global_path());
+    let mut dependencies = Vec::<GlobalModule>::new();
+    collect_dependencies(&base_path, &mut dependencies, None)?;
+    print_table(&dependencies);
+    Ok(())
+}
 
-pub fn get_global_path() -> PathBuf {
+pub fn run_standard() -> Result<(), Error> {
+    let app_pjson = PjsonDetail::from(&Cli::get().path)?;
+    let dependencies = get_standard_deps(&app_pjson)?;
+    print_table(&dependencies);
+    print_completion_message(format!(
+        "\n{} matches found in version {} of {}.\n",
+        dependencies.len(),
+        app_pjson.version,
+        app_pjson.name,
+    ))?;
+    Ok(())
+}
+
+pub fn run_diff(path: &PathBuf) -> Result<(), Error> {
+    let app_pjson = PjsonDetail::from(&Cli::get().path)?;
+    let dependencies = get_standard_deps(&app_pjson)?;
+    let diff_path = get_node_modules_path(&path);
+    let diff_pjson = PjsonDetail::from(&path)?;
+    let mut diff_dependencies = Vec::<StandardModule>::new();
+    collect_dependencies(&diff_path, &mut diff_dependencies, Some(&diff_pjson))?;
+    let diffed_pairs = DiffedPair::get_pairs(&dependencies, &diff_dependencies);
+    print_table(&diffed_pairs);
+    Ok(())
+}
+
+fn get_standard_deps(app_pjson: &PjsonDetail) -> Result<Vec<StandardModule>, Error> {
+    let base_path = get_node_modules_path(&Cli::get().path);
+    let mut dependencies = Vec::<StandardModule>::new();
+    collect_dependencies(&base_path, &mut dependencies, Some(&app_pjson))?;
+    Ok(dependencies)
+}
+
+fn get_global_path() -> PathBuf {
     let mut node_path = which("node").unwrap();
     node_path.pop();
     node_path.pop();
@@ -27,18 +67,20 @@ pub fn get_global_path() -> PathBuf {
     node_path
 }
 
-pub fn get_node_modules_path(path: &PathBuf) -> PathBuf {
-        let mut base_path = PathBuf::from(path);
-        base_path.push("node_modules");
-        base_path
+fn get_node_modules_path(path: &PathBuf) -> PathBuf {
+    let mut base_path = PathBuf::from(path);
+    base_path.push("node_modules");
+    base_path
 }
 
-
-pub fn collect_dependencies<T: node_module::NodeModule + Default>(base_path: &PathBuf, dependencies: &mut Vec<T>, app_pjson: Option<&PjsonDetail>) -> Result<(), Error> {
+fn collect_dependencies<T: node_module::NodeModule + Default>(
+    base_path: &PathBuf,
+    dependencies: &mut Vec<T>,
+    app_pjson: Option<&PjsonDetail>,
+) -> Result<(), Error> {
     let node_modules = base_path.read_dir()?;
-    let cli = Cli::get();
 
-    let filter_re = Regex::new(&cli.filter).unwrap();
+    let filter_re = Regex::new(&Cli::get().filter).unwrap();
 
     for entry in node_modules {
         if let Ok(entry) = entry {
@@ -67,9 +109,11 @@ pub fn collect_dependencies<T: node_module::NodeModule + Default>(base_path: &Pa
     Ok(())
 }
 
-pub fn print_table<T: node_module::PrintTable>(dependencies: &Vec<T>) {
+fn print_table<T: node_module::PrintTable>(dependencies: &Vec<T>) {
     let mut table = Table::new();
-    if dependencies.len() == 0 { return; }
+    if dependencies.len() == 0 {
+        return;
+    }
 
     for dependency in dependencies {
         table.add_row(dependency.table_row());
@@ -77,7 +121,7 @@ pub fn print_table<T: node_module::PrintTable>(dependencies: &Vec<T>) {
     table.printstd();
 }
 
-pub fn print_completion_message(message: String) -> Result<(), Error> {
+fn print_completion_message(message: String) -> Result<(), Error> {
     let stdout = io::stdout();
     let mut handle = stdout.lock();
     handle.write_all(message.as_bytes())?;
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
 use exitfailure::ExitFailure;
 
-use library::{Cli, PjsonDetail, GlobalModule, StandardModule, DiffedPair};
-
 fn main() -> Result<(), ExitFailure> {
-    let cli = Cli::get();
-
+    let cli = library::Cli::get();
 
     if cli.global {
-        let base_path = library::get_node_modules_path(&library::get_global_path());
-        let mut dependencies = Vec::<GlobalModule>::new();
-        library::collect_dependencies(&base_path, &mut dependencies, None)?;
-        library::print_table(&dependencies);
+        library::run_global()?;
+    } else if let Some(path) = &cli.diff {
+        library::run_diff(&path)?;
     } else {
-        let base_path = library::get_node_modules_path(&cli.path);
-        let app_pjson = PjsonDetail::from(&cli.path)?;
-            let mut dependencies = Vec::<StandardModule>::new();
-            library::collect_dependencies(&base_path, &mut dependencies, Some(&app_pjson))?;
-        
-        if let Some(path) = &cli.diff {
-            let diff_path = library::get_node_modules_path(&path);
-            let diff_pjson = PjsonDetail::from(&path)?;
-            let mut diff_dependencies = Vec::<StandardModule>::new();
-            library::collect_dependencies(&diff_path, &mut diff_dependencies, Some(&diff_pjson))?;
-            let diffed_pairs = DiffedPair::get_pairs(&dependencies, &diff_dependencies);
-            library::print_table(&diffed_pairs);
-        } else {
-            library::print_table(&dependencies);
-            library::print_completion_message(format!(
-                "\n{} matches found in version {} of {}.\n",
-                dependencies.len(),
-                app_pjson.version,
-                app_pjson.name,
-            ))?;
-        }
+        library::run_standard()?;
     }
 
     Ok(())
